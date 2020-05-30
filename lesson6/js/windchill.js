01b2b4f118d71e02f257088bc7588247377ba011
MeiganKnows <fir19001@byui.edu>
2020-05-30 15:10:32 -0700

    final updates

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,5 @@
-var t = document.getElementById("temp");
-var s = document.getElementById("speed");
+var t = document.getElementById("temp").textContent;
+var s = document.getElementById("speed").textContent;
 var f;
 if (t < 50 && s > 3) {
     f = 35.74 + 0.6215*t -35.75 * Math.pow(s, 0.16) + 0.4275*t*Math.pow(s, 0.16);
@@ -7,4 +7,4 @@ if (t < 50 && s > 3) {
 else{
     f="N/A";
 }
-document.getElementById("chill").innerHTML= f;
+document.getElementById("chill").innerHTML= Math.round(f);
