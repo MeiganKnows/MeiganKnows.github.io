c0988f48133f0a6c5ef1d56178b5b1cf66ae3e91
MeiganKnows <fir19001@byui.edu>
2020-06-06 15:40:09 -0700

    gallery updates

--- lesson7/js/preston.js
+++ lesson7/js/preston.js
@@ -10,9 +10,3 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-//friday banner
-if (day[d.getDay()] == "Friday") {
-    document.getElementById("banner").style.display = "block";
-} else {
-    document.getElementById("banner").style.display = "none";
-}
