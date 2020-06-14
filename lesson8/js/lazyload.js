f498f46b4f2a12b6d75894344e8e8eb8aa61d4a3
MeiganKnows <fir19001@byui.edu>
2020-06-13 22:51:19 -0700

    Storm center updates

---
+++ lesson8/js/lazyload.js
@@ -0,0 +1,33 @@
+//find all the images with [data-src] properties
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0.5,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+//changes placeholder image to actual image when activated
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
---
+++ lesson8/js/preston.js
@@ -0,0 +1,12 @@
+//Today's date at the bottom of the footer
+var d = new Date();
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("curDate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " +
+    month[d.getMonth()] + " " + d.getFullYear();
+
+//hamburger menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
