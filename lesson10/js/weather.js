2583eabdc604b8e51163894d878131611cc357dc
MeiganKnows <fir19001@byui.edu>
2020-06-27 15:38:48 -0700

    homepage updates

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -9,17 +9,15 @@ fetch(currentURL)
         document.getElementById("temp").textContent = jsObject.main.temp_max.toFixed(0);
         document.getElementById("humidity").textContent = jsObject.main.humidity;
         document.getElementById("speed").textContent = jsObject.wind.speed.toFixed(0);
-        
+
     });
 
-
-
-
-
-
-
-
-
-
-
 const forcastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=2daa9d071489be411f57523f3d6ec725&units=imperial"
+
+fetch(forcastURL)
+    .then((response) =>response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        for(i=0, i<jsObject.length, i++)
+        if()
+    })
