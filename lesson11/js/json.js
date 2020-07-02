5568dc623b5be4596170a8161a0ea7bd34f23524
MeiganKnows <fir19001@byui.edu>
2020-07-01 22:35:27 -0700

    week 11 intial updates

---
+++ lesson11/js/json.js
@@ -0,0 +1,41 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        //console.table(jsonObject); //temporary checking for valid response and data parsing
+        const towndata = jsonObject["towns"];
+        for (let i = 0; i < towndata.length; i++) {
+            if (towndata[i].name == "Preston" || towndata[i].name == "Soda Springs" || towndata[i].name == "Fish Haven") {
+                console.log[towndata];
+                let card = document.createElement("section");
+                let data = document.createElement("div");
+                let h3 = document.createElement("h3");
+                let h4 = document.createElement("h4");
+                let p1 = document.createElement("p");
+                let p2 = document.createElement("p");
+                let p3 = document.createElement("p");
+                let img = document.createElement("img");
+
+                h3.textContent = towndata[i].name;
+                h4.textContent = towndata[i].motto;
+                p1.textContent = "Founded in " + towndata[i].yearFounded;
+                p2.textContent = "Population: " + towndata[i].currentPopulation;
+                p3.textContent = "Average Rainfall: " + towndata[i].averageRainfall;
+                img.setAttribute("src", "images/" + towndata[i].name + "500.jpg");
+                img.setAttribute("alt", "Beautiful picture of " + towndata[i].name);
+
+                data.appendChild(h3);
+                data.appendChild(h4);
+                data.appendChild(p1);
+                data.appendChild(p2);
+                data.appendChild(p3);
+                card.appendChild(data);
+                card.appendChild(img);
+
+                document.querySelector("div.towndata").appendChild(card);
+            }
+        }
+    });
---
+++ lesson11/js/weather.js
@@ -0,0 +1,41 @@
+const currentURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=2daa9d071489be411f57523f3d6ec725&units=imperial"
+
+fetch(currentURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById("currentTemp").textContent = jsObject.main.temp.toFixed(0);
+        document.getElementById("currentDesc").textContent = jsObject.weather[0].description;
+        document.getElementById("temp").textContent = jsObject.main.temp_max.toFixed(0);
+        document.getElementById("humidity").textContent = jsObject.main.humidity;
+        document.getElementById("speed").textContent = jsObject.wind.speed.toFixed(0);
+
+        var t = document.getElementById("currentTemp").textContent;
+        var s = document.getElementById("speed").textContent;
+        var f;
+        if (t <= 50 && s > 3) {
+            f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        } else {
+            f = "N/A";
+        }
+        document.getElementById("chill").innerHTML = Math.round(f);
+
+    });
+
+const forcastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=2daa9d071489be411f57523f3d6ec725&units=imperial";
+
+fetch(forcastURL)
+    .then((response) => response.json())
+    .then((jsObject2) => {
+        const fiveday = jsObject2.list.filter(x => x.dt_txt.includes("18:00:00"));
+        
+        for (i = 0; i < fiveday.length; i++) {
+           const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            let d = new Date(fiveday[i].dt_txt);
+            const imagesrc = "https://openweathermap.org/img/w/" + fiveday[i].weather[0].icon + ".png";
+            const imagealt = fiveday[i].weather[0].description;
+            document.getElementById("day"+ i).textContent = weekday[d.getDay()];
+            document.getElementById("icon"+ i).setAttribute("src", imagesrc);
+            document.getElementById("icon" + i).setAttribute("alt", imagealt);
+            document.getElementById("temp" + i).textContent = fiveday[i].main.temp;
+        }
+    });
