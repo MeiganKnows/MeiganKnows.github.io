ded24630466aaa9ed716d11c51cef26f8d80e913
MeiganKnows <fir19001@byui.edu>
2020-07-22 18:42:00 -0700

    final project updates

--- scoots/js/rentalinfo.js
+++ scoots/js/rentalinfo.js
@@ -1,9 +1,15 @@
-fetch(rentaldata.json)
-    .then((response)=> response.json())
+const data = "rentalinfo.json";
+
+fetch(data)
+    .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-        for(i=0; i<jsObject.length; i++){
+        for (i = 0; i < jsObject.length; i++) {
             document.getElementById("type" + i).textContent = jsObject[i].type;
+            document.getElementById("max" + i).textContent = jsOjject[i].maxperson;
+            document.getElementById("rhalf" + i).textContent = jsObject[i].reservehalf;
+            document.getElementById("rwhole" + i).textContent = jsObject[i].reservewhole;
+            document.getElementById("whlaf" + i).textContent = jsObject[i].walkinhalf;
+            document.getElementById("wwhole" + i).textContent = jsObject[i].walkinwhole;
         }
 
-    })
+    });
